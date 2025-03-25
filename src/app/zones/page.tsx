90df73537d3640dba0a9f4b59d6220df0b2b2dde
anup354 <anupkasula5@gmail.com>
2025-03-25 11:52:26 +0545

    chnages

--- src/app/zones/page.tsx
+++ src/app/zones/page.tsx
@@ -1,18 +1,33 @@
 import { AppSidebar } from "@/components/app-sidebar"
-
 import { SiteHeader } from "@/components/site-header"
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
+import data from "./data.json"
+import { ZoneDataTable } from "@/components/zone-data-table"
+import { useEffect } from "react"
+import { Payment, columns } from "./columns"
+import { DataTableDemo } from "./data-table"
 
 
+interface BannerZoneData {
+  id: string;
+  name: string;
+  is_active: boolean;
+  placesize_id: string;
+}
 
-export default function Anup() {
+
+export default async function Zone() {
+  
+
   return (
     <SidebarProvider>
       <AppSidebar variant="inset" />
       <SidebarInset>
         <SiteHeader />
-        <div className="flex flex-1 flex-col">
-          anup
+        <div className="flex flex-col flex-1">
+        <div className="container py-10 mx-auto">
+      <DataTableDemo  />
+    </div>
         </div>
       </SidebarInset>
     </SidebarProvider>

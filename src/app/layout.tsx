599a7245542bc9b5b672f62b32fa390eb77a9926
anup354 <anupkasula5@gmail.com>
2025-03-26 11:32:35 +0545

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,10 +28,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-  <AuthProvider>
-
-{children}
-</AuthProvider>      </body>
+        <AuthProvider>{children}</AuthProvider>{" "}
+      </body>
     </html>
   );
 }
